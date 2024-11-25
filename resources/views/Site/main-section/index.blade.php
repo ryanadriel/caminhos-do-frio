5bc93994e77115b9bfbea35e84dbb9d51b9e208f
Adriel Ryan <adrielryan.tj@hotmail.com>
2024-11-25 19:14:37 -0300

    Atualizando seção principal do site

--- resources/views/Site/main-section/index.blade.php
+++ resources/views/Site/main-section/index.blade.php
@@ -6,14 +6,13 @@
     <div class="container">
         <div class="row align-items-center">
             <div class="col-md-5 col-lg-6 order-0 order-md-1 text-end"><img class="pt-7 pt-md-0 hero-img"
-                                                                            src="{{url('images/hero/hero-img.png')}}"
+                                                                            src="{{url('images/hero/main-image.png')}}"
                                                                             alt="hero-header"/></div>
             <div class="col-md-7 col-lg-6 text-md-start text-center py-6">
                 <h4 class="fw-bold text-danger mb-3">Descubra os Destinos da Serra Paraibana</h4>
                 <h1 class="hero-title">Explore e viva uma experiência inesquecível.</h1>
                 <p class="mb-4 fw-medium">Roteiros pensados para quem busca história, aventura e charme. <br
-                        class="d-none d-xl-block"/>Descubra trilhas, engenhos, cachoeiras que só o interior da Paraíba pode oferecer.<br
-                        class="d-none d-xl-block"/>sell they west hard for the.</p>
+                        class="d-none d-xl-block"/>Descubra trilhas, engenhos, cachoeiras que só o interior da Paraíba <br>pode oferecer.
                 <div class="text-center text-md-start"><a
                         class="btn btn-primary btn-lg me-md-4 mb-3 mb-md-0 border-0 primary-btn-shadow" href="#destination"
                         role="button">Conheça</a>
