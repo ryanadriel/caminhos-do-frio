e181e33491e601e9229f84c469ac8cdd807cad60
Adriel Ryan <adrielryan.tj@hotmail.com>
2024-11-29 12:54:44 -0300

    Bug migration Roles

--- database/migrations/2024_11_29_003442_create_roles_table.php
+++ database/migrations/2024_11_29_003442_create_roles_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
 
         Schema::create('role_user', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->fo('role_id')->constrained()->onDelete('cascade');
+            $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
             $table->timestamps();
